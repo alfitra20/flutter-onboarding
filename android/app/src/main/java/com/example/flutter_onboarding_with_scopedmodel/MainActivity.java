e837cc9a2d00d9dafa634ff67203af3f8a31fed1
alfitra20 <alfitrarahman@gmail.com>
2019-07-09 16:35:57 +0200

    initialize project

---
+++ android/app/src/main/java/com/example/flutter_onboarding_with_scopedmodel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.flutter_onboarding_with_scopedmodel;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
