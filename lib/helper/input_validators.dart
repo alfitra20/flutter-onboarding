fdf6bec84be43f3bec69b4cf0f4c36ede4d9abc6
alfitra20 <alfitrarahman@gmail.com>
2019-07-10 02:37:22 +0200

    fix spacing at input validators

--- lib/helper/input_validators.dart
+++ lib/helper/input_validators.dart
@@ -3,20 +3,20 @@ import 'package:flutter_onboarding_with_scopedmodel/assets/strings/strings.dart'
 class InputValidators {
 
   static String validateEmail(String value) {
-  Pattern emailPattern = Strings.emailPattern;  
-  RegExp emailRegex = new RegExp(emailPattern);
-  if (!emailRegex.hasMatch(value)) {
-    return Strings.invalidEmailAddressText;
+    Pattern emailPattern = Strings.emailPattern;  
+    RegExp emailRegex = new RegExp(emailPattern);
+    if (!emailRegex.hasMatch(value)) {
+      return Strings.invalidEmailAddressText;
+    }
+    return null;
   }
-  return null;
-}
 
-static String validatePassword(String value) {
-  if (value.length >= 15) {
-    return Strings.invalidPasswordText;
-  } else if (value.length < 6) {
-    return Strings.emptyPasswordText;
+  static String validatePassword(String value) {
+    if (value.length >= 15) {
+      return Strings.invalidPasswordText;
+    } else if (value.length < 6) {
+      return Strings.emptyPasswordText;
+    }
+    return null;
   }
-  return null;
-}
 }
